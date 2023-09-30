ea617a7f6ebd90a1f3acf08f7fb7ee3df993cba7
Azim <moazim555@gmail.com>
2023-10-01 00:19:23 +0530

    added bg img, span text ,modified navbar

---
+++ script.js
@@ -0,0 +1,22 @@
+//navbar 
+const navE =document.getElementById('navbar');
+     window.addEventListener('scroll',() =>{
+      if(window.scrollY>=56){
+        navE.classList.add('navbar-scrolled');
+      }else if(window.scrollY<56){
+        navE.classList.remove('navbar-scrolled');
+      }
+     });
+
+     // typed.js for Intr
+     var typed = new Typed('#intro', {
+        strings: ['Hi! My name is AZIM'],
+        typeSpeed: 150,
+      });
+// typed.js for developer
+     var typed = new Typed('#element', {
+        strings: ['Software Developer','Web Developer','FullStack Developer'],
+        typeSpeed: 80,
+        backSpeed:50,
+        loop:true
+      });
